a9e448a1ebb7f52da646ab5e7d15bcc21bfb9454
Amer <s109802@ap.be>
2021-02-02 18:42:58 +0100

    exam

---
+++ tensorf-test.py
@@ -0,0 +1,28 @@
+	#!/usr/bin/env python3
+import tensorflow as tf 
+import pathlib
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+np.set_printoptions(precision=4)
+dataset = tf.data.Dataset.from_tensor_slices([8, 3, 0, 8, 2, 1])
+dataset
+
+for elem in dataset:
+  print(elem.numpy())
+
+dataset1 = tf.data.Dataset.from_tensor_slices(tf.random.uniform([4, 10]))
+print(dataset1.element_spec)
+dataset2 = tf.data.Dataset.from_tensor_slices(
+   (tf.random.uniform([4]),
+    tf.random.uniform([4, 100], maxval=100, dtype=tf.int32)))
+
+print(dataset2.element_spec)
+
+dataset3 = tf.data.Dataset.from_tensor_slices(
+    tf.random.uniform([4, 10], minval=1, maxval=10, dtype=tf.int32))
+print(dataset3)
+for z in dataset3:
+	print(z.numpy())
